21e2b2b0b0a731bb07aafdbdd357d5597be17792
millnitzluan <luanmillnitz@gmail.com>
2018-11-09 18:04:45 -0200

    Created Badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
 import Badge from '@material-ui/core/Badge'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = {
-  default: {
-    backgroundColor: '#4360df'
+  normal: {
+    backgroundColor: '#4360DF',
+    left: '0px',
+    height: '20px',
+    width: '20px'
   },
   large: {
-    height: '50px',
-    widht: '50px'
-  },
-  normal: {
+    backgroundColor: '#4360DF',
+    left: '0px',
     height: '25px',
-    widht: '25px'
+    width: '25px'
   }
 }
 
-function CustomizedBadge(props) {
-  const { classes, count,  large, customSize, children } = props
-  const size = large ? classes.large : classes.normal
-  console.log(size)
-
+function CustomizedBadge({ classes, count,  large }) {
   return (
     <IconButton aria-label="Cart">
-      <Badge badgeContent={count} color="primary" className={ badge: classes.badgen }} />
+      <Badge badgeContent={count} color="primary" classes={{ badge: large ? classes.large : classes.normal }} />
     </IconButton>
   )
 }
