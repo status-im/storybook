be8bdfa2a02a7c964a35376024364ce724199dbd
millnitzluan <luanmillnitz@gmail.com>
2018-11-28 00:11:45 -0200

    Created cell item

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
 import { addDecorator, configure } from '@storybook/react';
+import withBackgrounds from "@storybook/addon-backgrounds";
+
+addDecorator(
+  withBackgrounds([
+    { name: 'default', value: '#eef2f5', default: true }
+  ])
+);
 
 function loadStories() {
   require('../src/stories');
